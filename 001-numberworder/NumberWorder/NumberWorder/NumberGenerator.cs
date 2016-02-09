72f4583124539c8ab972566f14571beae7e1c5da
Gaurav Badhan <gaurav.badhan@readify.net>
2016-02-09 14:52:39 +1000

    Added exercise 1

---
+++ 001-numberworder/NumberWorder/NumberWorder/NumberGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberWorder
+{
+    public class NumberGenerator
+    {
+        private Dictionary<int, string> numberWords;
+        public NumberGenerator()
+        {
+            numberWords = new Dictionary<int, string>();
+            numberWords.Add(0, "ZERO");
+            numberWords.Add(1, "ONE");
+            numberWords.Add(2, "TWO");
+            numberWords.Add(3, "THREE");
+            numberWords.Add(4, "FOUR");
+            numberWords.Add(5, "FIVE");
+            numberWords.Add(6, "SIX");
+            numberWords.Add(7, "SEVEN");
+            numberWords.Add(8, "EIGHT");
+            numberWords.Add(9, "NINE");
+        }
+        private string GetWord(long number)
+        {            
+            return numberWords[(int)number];
+        }
+
+        public string[] GetConvertedNumberArray(long number, string[] numberArray, int index)
+        {
+            numberArray[index] = GetWord(number % 10);
+            if (number <= 9)
+            {
+                return numberArray;
+            }
+
+            return GetConvertedNumberArray(number / 10, numberArray, index - 1);
+        }
+    }
+}
---
+++ 001-numberworder/NumberWorder/NumberWorder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NumberWorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Please pass a positive number as argument. Usage example: NumberWorder.exe 1234");
+            }
+            else
+            {
+                string inputArgument = args[0];
+                long input;
+                
+                if (long.TryParse(inputArgument, out input))
+                {
+                    if (input < 0)
+                    {
+                        Console.WriteLine("The argument can only be a positive number");
+                    }
+                    else
+                    {
+                        int numberOfDigits = inputArgument.Length;
+                        string[] numberArray = new string[numberOfDigits];
+                        NumberGenerator generator = new NumberGenerator();
+                        numberArray = generator.GetConvertedNumberArray(input, numberArray, numberOfDigits - 1);
+                        for (int count = 0; count < numberOfDigits; count++)
+                        {
+                            Console.Write(numberArray[count]);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The argument can only be a positive whole number with max value = 8999999999999999999");
+                }
+            }
+        }
+    }
+}
